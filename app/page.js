75aaf4d3a0a5831d630b6810a9278d47ea3f8675
Suraj <suraj.ukumar.p@gmail.com>
2024-08-02 17:53:15 +0530

    feat: The basic panry app is created with the help of bill zhang.

--- app/page.js
+++ app/page.js
@@ -1,12 +1,165 @@
-import{ Box} from '@mui/material'; 
+'use client'
+import Image from 'next/image'
+import {useState, useEffect} from 'react'
+import {firestore} from '@/firebase'
+import { Box, Button, Modal, Stack, TextField, Typography } from '@mui/material'; 
+import { collection, getDocs, query, setDoc, doc, getDoc, deleteDoc } from 'firebase/firestore';
 
 export default function Home() {
-  return <Box
-  width = "100vw"
-  height = "100vh"
-  display = "flex"
-  justifyContent = "center"
-  alignItems = "center"  
-  >  Hello world      
-  </Box>
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState('');
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+    docs.forEach(doc => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      });
+    });
+    setInventory(inventoryList);
+  };
+
+  // Add the item
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      await setDoc(docRef, {
+        quantity: quantity + 1 
+      });
+    } else {
+      await setDoc(docRef, {
+        quantity: 1
+      });
+    }
+    await updateInventory();
+  };
+
+  // Remove the item
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      if (quantity === 1) {
+        await deleteDoc(docRef);
+      } else {
+        await setDoc(docRef, {
+          quantity: quantity - 1
+        });
+      }
+    }
+    await updateInventory();
+  };
+
+  useEffect(() => {
+    updateInventory();
+  }, []);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap={2}
+    >
+      <Modal open={open} onClose={handleClose}>
+        <Box 
+          position="absolute"
+          top="50%"
+          left="50%"
+          width={400}
+          bgcolor="white"
+          border="2px solid #000"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}    
+          sx={{
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
+          <Typography variant='h6'>Add Item</Typography>
+          <Stack width="100%" direction="row" spacing={2}></Stack>
+          <TextField
+            variant="outlined"
+            fullWidth
+            value={itemName}
+            onChange={(e) => setItemName(e.target.value)}    
+          />
+          <Button
+            variant="outlined"
+            onClick={() => {
+              addItem(itemName);
+              setItemName('');
+              handleClose();
+            }}
+          >
+            Add
+          </Button>
+        </Box>
+      </Modal>
+
+      <Button variant="outlined" onClick={handleOpen}>Add New Item</Button>
+
+      <Box border="1px solid #333">
+      <Box
+        width="800px"
+        height="100px"
+        bgcolor="#ADD8E6"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Typography variant="h2" color="#333">Inventory Items</Typography>
+      </Box>
+
+      <Stack
+        width="800px"
+        height="300px"
+        spacing={2}
+        overflow="auto"
+      >
+        {inventory.map(({ name, quantity }) => (
+          <Box
+            key={name}
+            width="100%"
+            height="150px"
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            bgcolor="#F0F8FF"
+            padding={5}
+          >
+            <Typography variant='h3' color="#333" textAlign="center">
+              {name.charAt(0).toUpperCase() + name.slice(1)}
+            </Typography>
+            <Typography variant='h3' color="#333" textAlign="center">
+              {quantity}
+            </Typography>
+
+            <Button variant="outlined" onClick={() => removeItem(name)}>Remove</Button>
+
+
+
+          </Box>
+        ))}
+      </Stack>
+      </Box>
+    </Box>
+  );
 }
---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA1ECQ5Il_MGYTlTOyJzuxQcOnTzMsCPX4",
+  authDomain: "inventory-management-412f4.firebaseapp.com",
+  projectId: "inventory-management-412f4",
+  storageBucket: "inventory-management-412f4.appspot.com",
+  messagingSenderId: "522407320879",
+  appId: "1:522407320879:web:346462fa18b9608d7115da",
+  measurementId: "G-RJZGSLC21G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export {firestore};
